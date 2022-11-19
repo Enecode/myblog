31ea87e8a968549582a7e6cd7f96e6fc8cab21d6
Isah Jacob <isahjacob0@gmail.com>
2022-11-19 15:16:27 +0100

    update for deploy

--- blog/settings.py
+++ blog/settings.py
@@ -68,7 +68,7 @@ ROOT_URLCONF = 'blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'blogs/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -130,6 +130,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+#this code
+STATICFILES_DIRS = [
+    BASE_DIR / "blogs/templates/blogs/static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
