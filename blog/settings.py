82c4ad08cd4743303168f599787c257f73abf04e
Isah Jacob <isahjacob0@gmail.com>
2022-11-25 08:48:04 +0100

    update

--- blog/settings.py
+++ blog/settings.py
@@ -130,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+# STATICFILES_DIRS = [BASE_DIR / 'static']
 
 #this code
 STATICFILES_DIRS = [
